605b58f46da3cd653a9ece65a1d98cc9e222b34d
Shantel <snchristian8@gmail.com>
2021-11-22 22:29:51 -0500

    commit tree 2f12a06a42b24d50f18c89988d378e20606a50b7

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    if (timeRemaining===0){
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+else{
+  let timer= setTimeout(() => {
+      setTimeRemaining(timeRemaining -1)
+    }, 1000);
+  return ()=> clearTimeout(timer);
+  }
+    
+  },[timeRemaining,onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
